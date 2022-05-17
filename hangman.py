3653bdef3ef6fc6f269ac31756b7b736e2fbd164
Rituj Chaudhary <RChaudhary@infinera.com>
2022-05-17 22:24:32 +0530

    hangman 3

---
+++ hangman.py
@@ -0,0 +1,64 @@
+#Step 2
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#TODO-1: - Create an empty List called display.
+#For each letter in the chosen_word, add a "_" to 'display'.
+#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+empty_list =[]
+
+for x in range(0,len(chosen_word)):
+  empty_list.append("_")
+
+# guess = input("Guess a letter: ").lower()
+#print(empty_list)
+
+#TODO-2: - Loop through each position in the chosen_word;
+#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+x=0
+##########################my way###################
+# for letter in chosen_word:
+#   if letter == guess:
+#     empty_list[x] = guess
+#     x+=1
+#   else:
+#     x+=1
+
+# print(empty_list)
+##########################my way###################  
+
+# for position in range(len(chosen_word)):
+#   letter = chosen_word[position]
+#   if letter == guess:
+#     empty_list[position] = guess
+
+# print(empty_list)
+    
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
+#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+
+game = False
+
+while not game:
+  guess = input("Guess a letter: ").lower()
+  for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+        empty_list[position] = letter
+  print(empty_list)
+  
+  if '_' not in empty_list:
+    game = True
+    print("you win!!")
